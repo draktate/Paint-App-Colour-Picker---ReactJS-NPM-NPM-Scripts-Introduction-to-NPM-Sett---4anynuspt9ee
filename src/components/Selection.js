e8cc4f5f0494cf2e7ed56eba7a11ace77df18d89
draktate <draktate@gmail.com>
2022-09-25 12:40:49 +0530

    Initial build

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import '../styles/Child.css';
+import React, { useState } from "react";
+
+const Selection = (props) => {
+let [style, setStyle] = useState({ background: "" });
+
+ let applyColor= props.applyColor;
+ let selectNextBackground = props.selectNextBackground;
+ //console.log(applyColor)
+ 
+
+  return (
+    <div className='fix-box' style={style}  onClick={()=>applyColor(setStyle)}>
+      <h2 className="subheading">Selection</h2> 
+    </div>
+  )
+}
+export default Selection;
 
